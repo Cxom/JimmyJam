e65bdce4a105680cd866e32c95765ed10e131526
Cxom <cxomtdoh@yahoo.com>
2021-01-28 05:44:11 -0600

    Add ability to pick up interactables
    This caused a couple bugs - leafblower particles don't rotate, and leaf
    blower doesn't aim with camera

--- Assets/Scripts/InteractController.cs
+++ Assets/Scripts/InteractController.cs
@@ -4,7 +4,11 @@ using UnityEngine;
 
 public class InteractController : MonoBehaviour
 {
-    [SerializeField] private Interactable heldItem = null;
+    [SerializeField] private Transform playerHand;
+    [SerializeField] private float reach = 100;
+    
+    private Interactable heldItem = null;
+    
     
     void Start()
     {
@@ -13,35 +17,64 @@ public class InteractController : MonoBehaviour
 
     void Update()
     {
-        if (heldItem)
+        var interactedWithItem = heldItem;
+        if (!heldItem)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out RaycastHit hit, reach))
+            {
+                // TODO use tags for this not recursive component searching
+                Interactable interactable = hit.transform.GetComponentInParent<Interactable>();
+                if (interactable)
+                {
+                    if (Input.GetButtonDown("Use_Primary") && interactable.CanBePickedUp)
+                    {
+                        PickUpItem(interactable);
+                    } 
+                    interactedWithItem = interactable;
+                }
+            }
+        }
+
+        if (interactedWithItem)
         {
             // Primary
             if (Input.GetButtonDown("Use_Primary"))
             {
-                heldItem.PrimaryDown();
+                interactedWithItem.PrimaryDown();
             }
             if (Input.GetButton("Use_Primary"))
             {
-                heldItem.PrimaryHold();
+                interactedWithItem.PrimaryHold();
             }
             if (Input.GetButtonUp("Use_Primary"))
             {
-                heldItem.PrimaryUp();
+                interactedWithItem.PrimaryUp();
             }
             // Secondary
             if (Input.GetButtonDown("Use_Secondary"))
             {
-                heldItem.SecondaryDown();
+                interactedWithItem.SecondaryDown();
             }
             if (Input.GetButton("Use_Secondary"))
             {
-                heldItem.SecondaryHold();
+                interactedWithItem.SecondaryHold();
             }
             if (Input.GetButtonUp("Use_Secondary"))
             {
-                heldItem.SecondaryUp();
+                interactedWithItem.SecondaryUp();
             }
         }
 
+    }
+    
+    private void PickUpItem(Interactable interactable)
+    {
+        heldItem = interactable;
+        interactable.transform.SetParent(playerHand);
+        interactable.transform.position = interactable.offset;
+        interactable.transform.rotation = Quaternion.Euler(interactable.rotation);
+        interactable.transform.localScale = interactable.scale;
     }
 }
--- Assets/Scripts/Interactable.cs
+++ Assets/Scripts/Interactable.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public abstract class Interactable : MonoBehaviour
 {
-    [Header("Relative Transform When In Hand")] 
-    [SerializeField] public Vector3 offset;
-    [SerializeField] public Vector3 rotation;
-    [SerializeField] public Vector3 scale;
+    [SerializeField] private bool canBePickedUp;
+    public bool CanBePickedUp => canBePickedUp;
+
+    [Header("Relative Transform When In Hand")]
+
+    [SerializeField] public Vector3 offset = Vector3.zero;
+    [SerializeField] public Vector3 rotation = Vector3.zero;
+    [SerializeField] public Vector3 scale = new Vector3(1, 1, 1);
 
     public virtual void PrimaryDown()
     {
